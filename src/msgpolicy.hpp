4a44224e37e2c685d473aad60f73e4da9b95f85d
Danila V. Demidow <dandemidow@gmail.com>
2015-12-07 13:29:29 +0300

    add a template exception wrapper

--- src/msgpolicy.hpp
+++ src/msgpolicy.hpp
@@ -5,88 +5,114 @@
 
 namespace msg {
 
-struct MsgPolicy {
-  bool push(const msgblk_t &msg,error &err) {
+  struct MsgPolicy {
+    // push
+    bool push(const msgblk_t &msg, error &err) noexcept {
       int result = push_msg_copy(mem, bank, const_cast<msgblk_t*>(&msg), 0);
       err.set(result);
       return result >= 0;
-  }
-  bool push_test(const msgblk_t &msg,error &err) {
-      int result = push_msg_copy(mem, bank, const_cast<msgblk_t*>(&msg), 1);
-      err.set(result);
-      return result >= 0;
-  }
-  bool push(const msgblk_t &msg) {
-      error err;
-      int result = push(msg,err);
-      if(err)
-         throw exception(err);
-      return result >= 0;
-  }
-  bool push_test(const msgblk_t &msg) {
-      error err;
-      int result = push_test(msg,err);
-      if(err)
-         throw exception(err);
-      return result >= 0;
-  }
-  bool push(msgblk_t *msg,error &err) noexcept {
+    }
+
+    bool push(const msgblk_t &msg) throw(exception) {
+      return _msg_arg_exception<decltype(msg)>(&MsgPolicy::push, msg);
+    }
+
+    bool push(msgblk_t *msg, error &err) noexcept {
       int result = push_msg(mem, bank, msg, 0);
       err.set(result);
       return result >= 0;
-  }
-  bool push_test(msgblk_t *msg,error &err) {
-      int result = push_msg(mem, bank, msg, 1);
+    }
+
+    bool push(msgblk_t *msg) throw(exception) {
+      return _msg_arg_exception<decltype(msg)>(&MsgPolicy::push, msg);
+    }
+
+    bool push_test(const msgblk_t &msg, error &err) {
+      int result = push_msg_copy(mem, bank, const_cast<msgblk_t*>(&msg), 1);
       err.set(result);
       return result >= 0;
-  }
-  bool push(msgblk_t *msg) throw(exception) {
-      error err;
-      int result = push(msg,err);
-      if(err)
-         throw exception(err);
-      return result >= 0;
-  }
-  bool push_test(msgblk_t *msg) {
+    }
+
+    bool push_test(const msgblk_t &msg) {
       error err;
       int result = push_test(msg,err);
       if(err)
-         throw exception(err);
+        throw exception(err);
       return result >= 0;
-  }
-
-  msgblk_t *pop() {
-    return pop_msg(mem, bank, 0, NULL);
-  }
-  msgblk_t *pop_test() {
-    return pop_msg(mem, bank, 1, NULL);
-  }
-  msgblk_t *prep() {
-    error err;
-    auto msg = prep(err);
-    if (err) {
-      throw exception(err);
     }
-    return msg;
-  }
-  msgblk_t *prep(error &err) {
-    int status;
-    auto msg = prep_msg(mem, &status);
-    if ( msg ) return msg;
-    err.set(status);
-    return NULL;
-  }
-  void free(msgblk_t *msg) { if(msg)free_msg(mem, msg); }
 
-  MsgPolicy(shared_mem_t *mem, msg_bank_t *bank) :
-    mem(mem), bank(bank) {}
-private:
-  shared_mem_t *mem;
-  msg_bank_t *bank;
-};
+    bool push_test(msgblk_t *msg, error &err) {
+      int result = push_msg(mem, bank, msg, 1);
+      err.set(result);
+      return result >= 0;
+    }
 
-template <class M> struct trait;
-template <> struct trait<msgblk_t> { typedef MsgPolicy type; };
+    bool push_test(msgblk_t *msg) {
+      error err;
+      int result = push_test(msg,err);
+      if(err)
+        throw exception(err);
+      return result >= 0;
+    }
+
+    // pop
+    msgblk_t *pop(error &err) noexcept {
+      int status;
+      auto msg = pop_msg(mem, bank, 0, &status);
+      err.set(status);
+      return msg;
+    }
+
+    msgblk_t *pop() throw(exception) {
+      return _msg_void_exception(&MsgPolicy::pop);
+    }
+
+    msgblk_t *pop_test() {
+      return pop_msg(mem, bank, 1, NULL);
+    }
+
+    //prep
+    msgblk_t *prep(error &err) noexcept {
+      int status;
+      auto msg = prep_msg(mem, &status);
+      err.set(status);
+      return msg;
+    }
+
+    msgblk_t *prep() throw(exception) {
+      return _msg_void_exception(&MsgPolicy::prep);
+    }
+
+    void free(msgblk_t *msg) noexcept {
+      if ( msg ) free_msg(mem, msg);
+    }
+
+    MsgPolicy(shared_mem_t *mem, msg_bank_t *bank) :
+      mem(mem), bank(bank) {}
+  private:
+    shared_mem_t *mem;
+    msg_bank_t *bank;
+
+    template <class MSG, class F>
+    F _msg_arg_exception(F (MsgPolicy::*f)(MSG, error&), MSG msg) throw(exception) {
+      error err;
+      auto result = (this->*f)(msg, err);
+      if ( err ) throw exception(err);
+      return result;
+    }
+
+    template <class F>
+    F _msg_void_exception(F (MsgPolicy::*f)(error&)) throw(exception) {
+      error err;
+      auto result = (this->*f)(err);
+      if ( err ) throw exception(err);
+      return result;
+    }
+
+  };
+
+  template <class M> struct trait;
+  template <> struct trait<msgblk_t> { typedef MsgPolicy type; };
 }
 #endif // _MSGPOLICY_H_
 
