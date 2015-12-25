0be2f458d7f3c03911ba63c01fa49be146bd5336
Danila V. Demidow <dandemidow@gmail.com>
2015-12-25 14:04:28 +0300

    exception alias request

--- src/msgpolicy.hpp
+++ src/msgpolicy.hpp
@@ -8,6 +8,13 @@
 
 namespace msg {
 
+#define EXC_ALIAS(exception) \
+  template<class F, class ...Arg> \
+  auto _exc_alias(F &&f, Arg ...a) -> \
+  decltype(_add_exception::exc<F,exception>(this, std::forward<F>(f), a...)) { \
+    return _add_exception::exc<F,exception>(this, std::forward<F>(f), a...); \
+  }
+
   struct MsgBasic {
     MsgBasic(std::shared_ptr<shared_mem_t*> mem, const msg_bank_t *const bank) :
       mem(mem), bank(bank) {}
@@ -51,7 +58,8 @@ namespace msg {
     }
 
     msgblk_t pop_test() throw(exception,memory_deleted_exception){
-      return _add_exception::exc<PopType,ExceptionType>(this, &MsgPolicy::pop_test);
+      //return _add_exception::exc<PopType,ExceptionType>(this, &MsgPolicy::pop_test);
+      return _exc_alias<PopType>(&MsgPolicy::pop_test);
     }
 
     MsgPolicy(std::shared_ptr<shared_mem_t*> mem, const msg_bank_t *const bank) : MsgBasic(mem, bank) {}
@@ -78,6 +86,7 @@ namespace msg {
         err.set(result);
         return result >= 0;
     }
+    EXC_ALIAS(exception)
   };
 
   struct MsgPolicyRaw : public MsgBasic {
@@ -88,8 +97,10 @@ namespace msg {
     bool push(error &err, msgblk_t *msg) throw(memory_deleted_exception) {
         return push(err,msg,0);
     }
+
     bool push(msgblk_t *msg) throw(exception,memory_deleted_exception) {
-      return _add_exception::exc<PushType,ExceptionType>(this, &MsgPolicyRaw::push, msg);
+      //return _add_exception::exc<PushType,ExceptionType>(this, &MsgPolicyRaw::push, msg);
+      return _exc_alias<PushType>(&MsgPolicyRaw::push, msg);
     }
 
     bool push_test(error &err, msgblk_t *msg) throw(memory_deleted_exception){
@@ -156,7 +167,7 @@ namespace msg {
       err.set(result);
       return result >= 0;
     }
-
+    EXC_ALIAS(exception)
   };
 
   template <class M> struct trait;
